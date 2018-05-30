69e2b2c05333de05a6ccb8fb55118cf15a8dfec5
Eyunizal <emilio.yunizal@gmail.com>
2018-05-30 20:53:27 +0700

    script

--- scripts/master.js
+++ scripts/master.js
@@ -8,19 +8,49 @@
 //     var huruf = '';
 // }
         
-var boxKata = document.getElementById('boxKata');
-var jumlahHuruf = 5; // ganti dengan index dari array kata yang di pertanyakan
-for (var i = 0; i < jumlahHuruf; i++){
+// var boxKata = document.getElementById('boxKata');
+// var jumlahHuruf = 5; // ganti dengan index dari array kata yang di pertanyakan
+// for (var i = 0; i < jumlahHuruf; i++){
+//     var divHuruf = document.createElement('div');
+//     divHuruf.id = 'baris-' + i;
+//     divHuruf.className = 'box';
+//     boxKata.appendChild(divHuruf);
+// }
+
+var soal = [{
+    jawaban: 'Hijau',
+    hint: 'Meletus Balon'
+    },
+    {
+    jawaban: 'Kancil',
+    hint: 'Hewan Nakal'
+    },
+    {
+    jawaban: 'Singa',
+    hint: 'Raja Hutan'
+    }
+];
+
+
+function init(){
+    var acak = Math.floor(Math.random() * soal.length);
+    var jawaban = '';
+    var hint = '';
+    for (var i = 0; i< soal.length; i++){
+        if (acak === i){
+            jawaban = soal[i]['jawaban'];
+            hint = soal[i]['hint'];
+        }
+    }
+
+    var boxKata = document.getElementById('boxKata');
+    var jumlahHuruf = jawaban.length; 
+    for (var i = 0; i < jumlahHuruf; i++){
     var divHuruf = document.createElement('div');
     divHuruf.id = 'baris-' + i;
     divHuruf.className = 'box';
-    boxKata.appendChild(divHuruf);
+    }
+    return boxKata.appendChild(divHuruf);
 }
-
-var balonKu = ['hijau','kuning','kelabu', 'merah muda', 'biru'];
-
-
-        function acakKata(min, max){
-            var acak = Math.floor(Math.random() * balonku.length);
-            return acak
-        }
+init()
+   
